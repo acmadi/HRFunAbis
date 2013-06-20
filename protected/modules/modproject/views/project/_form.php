23f27f6ef35476c7b3aff8d2939a8c275d81cd55
afrishal <afrishal@gmail.com>
2013-06-20 14:59:07 +0700

    modifikasi beberapa UI project, translate label atribut

--- protected/modules/modproject/views/project/_form.php
+++ protected/modules/modproject/views/project/_form.php
@@ -47,7 +47,12 @@
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'version_date'); ?>
-		<?php echo $form->textField($model,'version_date'); ?>
+		<?php $this->widget('ext.my97DatePicker.JMy97DatePicker',array(
+					'name'=>CHtml::activeName($model,'version_date'),
+					'value'=>$model->version_date,
+					'options'=>array('dateFmt'=>'yyyy-MM-dd'),
+				));
+				?>
 		<?php echo $form->error($model,'version_date'); ?>
 	</div>
 
@@ -59,7 +64,12 @@
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'status_date'); ?>
-		<?php echo $form->textField($model,'status_date'); ?>
+		<?php $this->widget('ext.my97DatePicker.JMy97DatePicker',array(
+					'name'=>CHtml::activeName($model,'status_date'),
+					'value'=>$model->status_date,
+					'options'=>array('dateFmt'=>'yyyy-MM-dd'),
+				));
+				?>
 		<?php echo $form->error($model,'status_date'); ?>
 	</div>
 
@@ -71,25 +81,45 @@
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'plan_start_date'); ?>
-		<?php echo $form->textField($model,'plan_start_date'); ?>
+		<?php $this->widget('ext.my97DatePicker.JMy97DatePicker',array(
+					'name'=>CHtml::activeName($model,'plan_start_date'),
+					'value'=>$model->plan_start_date,
+					'options'=>array('dateFmt'=>'yyyy-MM-dd'),
+				));
+				?>
 		<?php echo $form->error($model,'plan_start_date'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'plan_end_date'); ?>
-		<?php echo $form->textField($model,'plan_end_date'); ?>
+		<?php $this->widget('ext.my97DatePicker.JMy97DatePicker',array(
+					'name'=>CHtml::activeName($model,'plan_end_date'),
+					'value'=>$model->plan_end_date,
+					'options'=>array('dateFmt'=>'yyyy-MM-dd'),
+				));
+				?>
 		<?php echo $form->error($model,'plan_end_date'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'act_start_date'); ?>
-		<?php echo $form->textField($model,'act_start_date'); ?>
+		<?php $this->widget('ext.my97DatePicker.JMy97DatePicker',array(
+					'name'=>CHtml::activeName($model,'act_start_date'),
+					'value'=>$model->act_start_date,
+					'options'=>array('dateFmt'=>'yyyy-MM-dd'),
+				));
+				?>
 		<?php echo $form->error($model,'act_start_date'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'act_end_date'); ?>
-		<?php echo $form->textField($model,'act_end_date'); ?>
+		<?php $this->widget('ext.my97DatePicker.JMy97DatePicker',array(
+					'name'=>CHtml::activeName($model,'act_end_date'),
+					'value'=>$model->act_end_date,
+					'options'=>array('dateFmt'=>'yyyy-MM-dd'),
+				));
+				?>
 		<?php echo $form->error($model,'act_end_date'); ?>
 	</div>
 
@@ -101,7 +131,12 @@
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'spmk_date'); ?>
-		<?php echo $form->textField($model,'spmk_date'); ?>
+		<?php $this->widget('ext.my97DatePicker.JMy97DatePicker',array(
+					'name'=>CHtml::activeName($model,'spmk_date'),
+					'value'=>$model->spmk_date,
+					'options'=>array('dateFmt'=>'yyyy-MM-dd'),
+				));
+				?>
 		<?php echo $form->error($model,'spmk_date'); ?>
 	</div>
 
@@ -117,7 +152,7 @@
 		<?php echo $form->error($model,'pic'); ?>
 	</div>
 
-	<div class="row">
+	<!-- <div class="row">
 		<?php echo $form->labelEx($model,'created_by'); ?>
 		<?php echo $form->textField($model,'created_by',array('size'=>50,'maxlength'=>50)); ?>
 		<?php echo $form->error($model,'created_by'); ?>
@@ -127,7 +162,7 @@
 		<?php echo $form->labelEx($model,'created_date'); ?>
 		<?php echo $form->textField($model,'created_date'); ?>
 		<?php echo $form->error($model,'created_date'); ?>
-	</div>
+	</div> -->
 
 	<div class="row buttons">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
--- protected/modules/modproject/views/project/admin.php
+++ protected/modules/modproject/views/project/admin.php
@@ -2,10 +2,10 @@
 /* @var $this ProjectController */
 /* @var $model Project */
 
-$this->breadcrumbs=array(
-	'Projects'=>array('index'),
+$this->widget('bootstrap.widgets.TbBreadcrumbs', array( 'links'=>array(
+	'Projects'=>array('index'), 
 	'Manage',
-);
+)));
 
 $this->menu=array(
 	array('label'=>'List Project', 'url'=>array('index')),
@@ -26,19 +26,21 @@ $('.search-form form').submit(function(){
 ");
 ?>
 
-<h1>Manage Projects</h1>
+<?php
+	$this->beginWidget('bootstrap.widgets.TbBox', array(
+	    'title' => 'Daftar Proyek',
+	    'headerIcon' => 'icon-home',
+	    'headerButtons' => array(
+			array(
+				'class' => 'bootstrap.widgets.TbButtonGroup',
+				'buttons'=>array(
+					array('label'=>'Tambah Proyek', 'url'=>array('/modproject/project/create')),
+				),
+			),
+	    ),
+	));		
+?>
 
-<p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
-</p>
-
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
-<div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
-</div><!-- search-form -->
 
 <?php $this->widget('zii.widgets.grid.CGridView', array(
 	'id'=>'project-grid',
@@ -72,3 +74,7 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 		),
 	),
 )); ?>
+
+<?php 
+	$this->endWidget();
+	?>
