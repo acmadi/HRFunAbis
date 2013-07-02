767b8cd97622dd3356d8b5da31f7585127421fcd
Dwi Priyanto <keindahan120288@yahoo.com>
2013-07-02 13:22:07 +0700

    update

--- protected/extensions/projectscurve/views/index.php
+++ protected/extensions/projectscurve/views/index.php
@@ -31,33 +31,3 @@ $this->widget('application.extensions.EFlot.EFlotGraphWidget',
 );
 
 ?>
-
-<?php
-	// $this->Widget('ext.ActiveHighcharts.HighchartsWidget', array(
-		// 'dataProvider'=>$dataProvider,
-		// 'template'=>'{items}',
-		// 'options'=> array(
-			// 'title'=>array(
-				// 'text'=>'S Curve'
-			// ),
-			// 'xAxis'=>array(
-				// // It cant be 1.a self-defined xAxis array as you want; 
-				// // 2.a series from datebase such as time series
-				// "categories"=>'period_week'            
-			// ),
-			// 'series'=>array(
-				// array(
-					// 'type'=>'line',
-					// 'name'=>'Actual',             //title of data
-					// 'dataResource'=>'progress_actual',     //data resource according to datebase column
-				// ),
-				// array(
-					// 'type'=>'line',
-					// 'name'=>'Plan',             //title of data
-					// 'dataResource'=>'progress_plan',     //data resource according to datebase column
-				// )
-			// )
-		// )
-	// ));
-?>
-
