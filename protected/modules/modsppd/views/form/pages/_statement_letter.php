8f0e2fb5fe4f4a039d4bddc1c58cd8acef560e84
Dwi Priyanto <keindahan120288@yahoo.com>
2013-07-15 09:56:36 +0700

    tambah dikit dasbor

--- protected/modules/modsppd/views/form/pages/_statement_letter.php
+++ protected/modules/modsppd/views/form/pages/_statement_letter.php
@@ -1,7 +1,5 @@
 <div class="span12">
-	<div class="span6">
-	
-	</div>
+	<embed src="<?php echo Yii::app()->baseUrl.$model->statement_letter;?>" width="970" height="275">
 	<div class="span6">
 	</div>	
 </div>	
